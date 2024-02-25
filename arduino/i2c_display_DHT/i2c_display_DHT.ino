baff631beef5d7f2ab4899c08bde2b9b8c6161e6
Kalle Lassila <kalle.lassila@protonmail.com>
2024-02-25 20:38:14 +0200

    Added humidity display.

--- arduino/i2c_display_DHT/i2c_display_DHT.ino
+++ arduino/i2c_display_DHT/i2c_display_DHT.ino
@@ -14,12 +14,14 @@ DHT20 DHT;
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);  // High speed I2C
 
 // U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    //Low spped I2C
+void print_display(void);
 
 void setup(void) {
     Serial.begin(115200);
+    Serial.print("jep\n");
     Wire.begin();
     DHT.begin();
-    
+
     u8g2.begin();
     u8g2.setDisplayRotation(U8G2_R2);
 }
@@ -28,9 +30,11 @@ void loop(void) {
     int status = DHT.read();
     float humidity = DHT.getHumidity();
     float temperature = DHT.getTemperature();
-
+    Serial.print("\n");
+    Serial.print("Humidity:\n");
     Serial.print(humidity, 1);
     Serial.print("\n");
+    Serial.print("Temperature:\n");
     Serial.print(temperature, 1);
     Serial.print("\n");
 
@@ -39,48 +43,27 @@ void loop(void) {
 
     char temp_str[6];
     char hum_str[6];
-    dtostrf(temperature, 4, 1, temp_str);
-    dtostrf(humidity, 4, 1, hum_str);
 
-    char temperature_line[30];
-    char humidity_line[30];
-    char bufferi[15];
-    snprintf(temperature_line, 30, "Temperature: %s", temp_str);
-    //snprintf(humidity_line, 30, "Humidity: %s", hum_str);
+    dtostrf(temperature, 5, 2, temp_str);
+    dtostrf(humidity, 5, 1, hum_str);
+
+    char temperature_line[20];
+    char humidity_line[20];
+
+    snprintf(temperature_line, 20, "Temperature: %s", temp_str);
+    snprintf(humidity_line, 20, "Humidity: %s", hum_str);
+    Serial.print(temperature_line);
+    Serial.print("\n");
     Serial.print(humidity_line);
+    Serial.print("\n");
 
-    
-    //u8g2.drawStr(0, 10, temperature_line);
-    //u8g2.drawStr(0, 20, humidity_line);
+    u8g2.drawStr(0, 10, temperature_line);
+    u8g2.drawStr(0, 20, humidity_line);
     u8g2.sendBuffer();
 
-    switch (status)
-    {
-      case DHT20_OK:
-        Serial.print("OK");
-        break;
-      case DHT20_ERROR_CHECKSUM:
-        Serial.print("Checksum error");
-        break;
-      case DHT20_ERROR_CONNECT:
-        Serial.print("Connect error");
-        break;
-      case DHT20_MISSING_BYTES:
-        Serial.print("Missing bytes");
-        break;
-      case DHT20_ERROR_BYTES_ALL_ZERO:
-        Serial.print("All bytes read zero");
-        break;
-      case DHT20_ERROR_READ_TIMEOUT:
-        Serial.print("Read time out");
-        break;
-      case DHT20_ERROR_LASTREAD:
-        Serial.print("Error read too fast");
-        break;
-      default:
-        Serial.print("Unknown error");
-        break;
-    }
-    Serial.print("\n");
-    delay(5000);  
-    }
+    delay(3000);  
+}
+
+void print_display(void){
+
+}
